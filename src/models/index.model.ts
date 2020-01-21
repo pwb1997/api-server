fa78406f90c6da46fb8f3013b359c9463b85fd37
pwb1997 <pwb1997@hotmail.com>
2020-01-21 18:58:18 +0800

    update index

--- src/models/index.model.ts
+++ src/models/index.model.ts
@@ -1,17 +1,17 @@
-import Archive from '@models/archive.model';
+import Archive from './archive.model';
 import Category from './category.model';
-import Company from '@models/company.model';
-import Country from '@models/country.model';
+import Company from './company.model';
+import Country from './country.model';
 import Media from './media.model';
 import MediaFolder from './media-folder.model';
 import MediaFolderAccess from './media-folder-access';
 import Price from './price.model';
-import PriceType from '@models/price-type.model';
+import PriceType from './price-type.model';
 import Product from './product.model';
 import ProductFolder from './product-folder';
 import ProductFolderAccess from './product-folder-access';
-import User from '@models/user.model';
-import UserGroup from '@models/user-group.model';
+import User from './user.model';
+import UserGroup from './user-group.model';
 
 const entities = [
   Archive,
